1cf3b7b02b9f80381e9b44bc6150ea5db816b211
Sunny Bharadwaj <sunny@chakradesign.co>
2019-12-10 00:12:48 +0530

    Page files and controller setup

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    function landing() {
+        return view('layout');
+    }
+}
